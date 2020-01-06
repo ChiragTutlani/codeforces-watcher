a3989da064a56d9d0b2e75dacc2d620ed6cebe10
Chirag <chirag161198@gmail.com>
2020-01-06 12:10:00 +0530

    Login Screen and AsyncStorage implemented with no statusBarView

--- src/screens/Homepage.js
+++ src/screens/Homepage.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { retrieveUserHandleAsync } from '../AsyncStorageMethods'
+import yourUserHandleActionCreator from '../redux/actions/yourUserHandleAction'
 
 class Homepage extends React.Component{
+    async componentDidMount(){
+        const data = await retrieveUserHandleAsync()
+        if(data.status==='ok'){
+            yourUserHandleActionCreator(data.userHandle)
+        }
+    }
+
     render(){
         return(
             <View>
